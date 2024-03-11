59b8fdab032142c0eecedfcc6f671bbfa92a52f0
daniel <dfreijo@uoc.edu>
2024-03-11 20:57:00 +0100

    Prueba projectos recientes

---
+++ js/data.js
@@ -0,0 +1,55 @@
+var userselected="0";
+var projectselected="0";
+   
+    
+// Defino los usuarios
+var userdata = [
+user1 = {"nombre":"Maria", "pass":"12345678", "active":1},
+user2 = {"nombre":"Carlos", "pass":"12345678", "active":1},
+user3 = {"nombre":"Esteban", "pass":"12345678", "active":1},
+user4 = {"nombre":"Daniel", "pass":"12345678", "active":1},
+user5 = {"nombre":"Jordi", "pass":"12345678", "active":1},
+];  
+
+// Defino el array de los Proyectos
+var projects = [
+        {       
+                "id":0, 
+                "name": "Project A",
+                "description":"This is a description of the Project A.",
+                "department":"Desarrollo",
+                "startdate":"2024-01-01",
+                "enddate":"2024-02-03",
+                "backgroundcolor":"FFFFFF",
+                "backgroundimage":"default.jpg",
+                "priority":0,
+                "status":1
+        },
+
+        {
+                "id":2, 
+                "name": "Project B",
+                "description":"This is a description of the Project B.",
+                "department":"Diseño",
+                "startdate":"2023-01-01",
+                "enddate":"2023-02-03",
+                "backgroundcolor":"FFFFFF",
+                "backgroundimage":"default.jpg",
+                "priority":0,
+                "status":1
+        },
+
+        {       
+                "id":1, 
+                "name": "Project C",
+                "description":"This is a description of the Project A.",
+                "department":"Marketing",
+                "startdate":"2024-03-01",
+                "enddate":"2024-05-03",
+                "backgroundcolor":"FFFFFF",
+                "backgroundimage":"default.jpg",
+                "priority":1,
+                "status":0
+        }
+];
+
--- js/index.js
+++ js/index.js
@@ -1,21 +1,31 @@
-$(document).ready(function() {
-    // Cuando se hace clic en el botón "Añadir elemento"
-    $('#addCardButton').on('click', function() {
-      // 
-      // Crear un nuevo elemento "card"
-      const newCard = `
-        <div class="card" style="width: ;">
-            <img src="https://via.placeholder.com/150" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Título de la tarjeta</h5>
-                <p class="card-text">Esta es una tarjeta de ejemplo para mostrar cómo agregar un nuevo elemento al listado.</p>
-                <a href="#" class="btn btn-primary">Ir a alguna parte</a>
-            </div>
-        </div>
-      `;
-  
-      // Agregar la nueva "card" al contenedor de tarjetas
-      $('#cardList').append(newCard);
-    });
+function createProjectCard(project) {
+  return `
+    <div class="card">
+      ${project.name}
+    </div>
+  `;
+}
 
+function showRecentProjects(projects) {
+  const recentProjects = $('#recentProjects');
+  recentProjects.empty();
+
+  projects.forEach(project => {
+    const cardHTML = createProjectCard(project);
+    recentProjects.append(cardHTML);
+  });
+}
+
+function showAllProjects(projects) {
+  const allProjects = $('#allProjects');
+  allProjects.empty();
+
+  projects.forEach(project => {
+    const cardHTML = createProjectCard(project);
+    allProjects.append(cardHTML);
+  });
+}
+
+$(document).ready(function() {
+  showRecentProjects(projects);
 });
