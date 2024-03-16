fa5eac28f8cf4a03c7ba441a9be7991d7c7cd765
CarlosEF2023 <cespigol@uoc.edu>
2024-03-16 16:50:51 +0100

    más cambios del interface1
    más cambios del interface1

--- js/interface1.js
+++ js/interface1.js
@@ -5,6 +5,74 @@ $(document).ready(function(event) {
     allProjects("ALL");
     recentProjects();
 
+    // --------------------------------------------------------------------------
+    // FUNCIONES
+    // --------------------------------------------------------------------------
+
+    // Función para definir la tarjeta del proyecto
+    // Entra el indice del proyecto a mostrar en la tarjeta
+    // Devuelve una variable de texto para insertar la en HTML
+    function defineCard(index){
+        var text='<div class="card" style="width: 18rem; background-color:' + projects[index]["backgroundcolor"] + ';">';
+        text +='<img class="card-img-top" src="../assets/' + projects[index]["backgroundimage"] + '" alt="Card image cap">';  
+        text +='<div class="card-img-overlay">';
+        text +='<h5 class="card-title">'+ projects[index]["name"];
+        if (projects[index]["status"]!=1){
+            // Si un proyecto esta completado no se puede eliminar.
+            text +='<a href="#" class="btn close-button deteleproject" data="'+ projects[index]["id"]+'">❌</a>'; 
+        };
+        text +='</h5>';
+        text +='<p class="card-text" style="width:100px;overflow-wrap: break-word;">';
+        text +=projects[index]["description"];
+        text +='</p>';
+        text +='</div>';
+        text +='<div class="card-footer" style="background-color:' + projects[index]["backgroundcolor"] + '">';
+        if (projects[index]["priority"]==0){
+            text +='<a href="#" class="btn priority" style="position:absolute;left:3px" data_project="'+ projects[index]["id"]+'">★</a>';
+        }else{
+            text +='<a href="#" class="btn priority" style="position:absolute;left:3px" data_project="'+ projects[index]["id"]+'">⭐</a>';
+        };
+        text +='<a href="#" class="btn editproject" style="position:relative;left:100px" data_project="'+ projects[index]["id"]+'">📝</a>';
+        text +='<a href="#" class="btn openproject" style="position:absolute;right:3px"  data_project="'+ projects[index]["id"]+'">➠</a>';
+        text +='</div>';
+        text +='</div>';
+        return text;
+    };
+
+    // Función para filtrar los proyectos recientes
+    //
+    function recentProjects(){
+        $('#RecentProjects').html("");
+        var v_startdate = new Date();
+        v_startdate.setHours(0,0,0,0); // Establece la hora a medianoche
+        $.each(projects, function(index, data) {
+            var v_enddate = new Date(data.startdate);  // Establecemos la fecha de incio del proyecto
+            var diff = Math.abs(v_startdate-v_enddate); // Diferencia de dias entre la fecha actual y la fecha de inicio del proyecto
+            var diff_days=diff/(1000*60*60*24); // Lo pasamos a dias
+            // Si es un proyecto de hace 30 dias o menos lo mostramos como reciente
+            if (diff_days<=30){
+                // Añadimos la tarjeta del proyecto a la vista de recientes
+                $('#RecentProjects').append(defineCard(index));
+            }
+        });
+    };
+
+    // Función para filtrar todos los proyectos
+    function allProjects(filter){
+        $('#AllProjects').html("");
+        $.each(projects, function(index, data) {
+            // Añadimos el Proyecto a la vista de todos.
+            if (filter=="ALL"){
+                $('#AllProjects').append(defineCard(index));
+            }else{
+                if (data.department == filter){
+                    $('#AllProjects').append(defineCard(index));
+                }
+            }
+            
+        });
+    };
+
     // -----------------------------------------------------------------------------------------------------
     // EVENTOS formularios PROJECTS
     // -----------------------------------------------------------------------------------------------------
@@ -99,8 +167,7 @@ $(document).ready(function(event) {
         }
 
         // Si todo está correcto podemos agregarlo al ARRAY
-        console.log(ErrorMSG.length);
-        if (ErrorMSG.length == 0){
+        if (ErrorMSG.length === 0){
             projects.push({"id":(projects.length + 1), 
             "name": $("#ProjectName").val(), 
             "description":$("#ProjectDescription").val(),
@@ -113,7 +180,7 @@ $(document).ready(function(event) {
             "status":$("#ProjectStatus").val()});
             $("#ModalForm").modal('hide');
             // Actualizo las listas de Proyectos
-            allProjects();
+            allProjects($("ProjecDepartment").val());
             recentProjects();
         } else {
             // Tiene que ser un modal de error
@@ -122,69 +189,6 @@ $(document).ready(function(event) {
         };
         e.stopPropagation();        
       });
-
-
-    // --------------------------------------------------------------------------
-    // PROYECTOS
-    // --------------------------------------------------------------------------
-
-    function defineCard(index){
-        var text='<div class="card" style="width: 18rem;">';
-        text +='<img class="card-img-top" src="../assets/' + projects[index]["backgroundimage"] + '" alt="Card image cap">';  
-        text +='<div class="card-img-overlay">';
-        text +='<h5 class="card-title">'+ projects[index]["name"];
-        if (projects[index]["status"]!=1){
-            // Si un proyecto esta completado no se puede eliminar.
-            text +='<a href="#" class="btn close-button deteleproject" data="'+ projects[index]["id"]+'">❌</a>'; 
-        };
-        text +='</h5>';
-        text +='<p class="card-text" style="width:100px;overflow-wrap: break-word;">';
-        text +=projects[index]["description"];
-        text +='</p>';
-        text +='</div>';
-        text +='<div class="card-footer" style="backgroundcolor:' + projects[index]["backgroundcolor"] + '">';
-        if (projects[index]["priority"]==0){
-            text +='<a href="#" class="btn priority" style="position:absolute;left:3px" data_project="'+ projects[index]["id"]+'">★</a>';
-        }else{
-            text +='<a href="#" class="btn priority" style="position:absolute;left:3px" data_project="'+ projects[index]["id"]+'">⭐</a>';
-        };
-        text +='<a href="#" class="btn editproject" style="position:relative;left:100px" data_project="'+ projects[index]["id"]+'">📝</a>';
-        text +='<a href="#" class="btn openproject" style="position:absolute;right:3px"  data_project="'+ projects[index]["id"]+'">➠</a>';
-        text +='</div>';
-        text +='</div>';
-        return text;
-    };
-
-    function recentProjects(){
-        $('#RecentProjects').html("");
-        var v_startdate = new Date();
-        v_startdate.setHours(0,0,0,0); // Establece la hora a medianoche
-        $.each(projects, function(index, data) {
-            var v_enddate = new Date(data.startdate);  // Establecemos la fecha de incio del proyecto
-            var diff = Math.abs(v_startdate-v_enddate); // Diferencia de dias entre la fecha actual y la fecha de inicio del proyecto
-            var diff_days=diff/(1000*60*60*24); // Lo pasamos a dias
-            // Si es un proyecto de hace 30 dias o menos lo mostramos como reciente
-            if (diff_days<=30){
-                // Añadimos la tarjeta del proyecto a la vista de recientes
-                $('#RecentProjects').append(defineCard(index));
-            }
-        });
-    };
-
-    function allProjects(filter){
-        $('#AllProjects').html("");
-        $.each(projects, function(index, data) {
-            // Añadimos el Proyecto a la vista de todos.
-            if (filter=="ALL"){
-                $('#AllProjects').append(defineCard(index));
-            }else{
-                if (data.department == filter){
-                    $('#AllProjects').append(defineCard(index));
-                }
-            }
-            
-        });
-    };
     
     $(".filterproject").on("click", function(e){
         var filterappliced = $(this).find("a").attr("dataproject");
